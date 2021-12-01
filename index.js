8d76b45d842ddd5d7d5cf2fd28cc548ef5a65d2d
Gareth Dwyer <sixhobbits@users.noreply.github.com>
2021-12-01 10:12:26 +0200

    fix redirect

--- index.js
+++ index.js
@@ -236,13 +236,10 @@ app.get('/teams-pro/interviewing-candidates-guest-mode', async (req, res) => {
     res.redirect(301, '/teams-pro/interviewing-candidates');
 });
 app.get('/pro/guests', async (req, res) => {
-    res.redirect(301, '/teams-pro/interviewing-candidates-guest-mode');
+    res.redirect(301, '/teams-pro/interviewing-candidates');
 });
 app.get('/repls/python-plots', async (req, res) => {
     res.redirect(301, '/tutorials/creating-interactive-python-plots-matplotlib');
-});
-app.get('/teams-pro/interviewing-candidates-guest-mode', async (req, res) => {
-    res.redirect(301, '/teams-pro/interviewing-candidates');
 });
 app.get('/tutorials/PinboardProjectPart1', async (req, res) => {
     res.redirect(301, '/tutorials/pinboard-project-part-1');
